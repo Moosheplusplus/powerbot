e794fe0b9977f0fd468261bd9937de1cfd987e4f
Mooshe <admin@mooshe.pw>
2016-07-07 10:33:16 -0700

    Support Genie and Drunken Dwarf in randoms

--- src/org/powerbot/bot/rt4/RandomEvents.java
+++ src/org/powerbot/bot/rt4/RandomEvents.java
@@ -6,12 +6,20 @@ import org.powerbot.script.rt4.ClientContext;
 import org.powerbot.script.rt4.Npc;
 
 public class RandomEvents extends PollingScript<ClientContext> {
+	
+	private enum State {
+		IDLE,
+		TALKING
+	}
+	
+	private State state = State.IDLE;
+	
 	public RandomEvents() {
 		priority.set(3);
 	}
 
 	private boolean isValid() {
-		return !ctx.npcs.select().within(5d).action("Dismiss").select(new Filter<Npc>() {
+		return state != State.IDLE || !ctx.npcs.select().within(5d).action("Dismiss").select(new Filter<Npc>() {
 			@Override
 			public boolean accept(final Npc npc) {
 				return npc.interacting().equals(ctx.players.local());
@@ -30,7 +38,30 @@ public class RandomEvents extends PollingScript<ClientContext> {
 		if (!threshold.contains(this)) {
 			threshold.add(this);
 		}
-
-		ctx.npcs.poll().interact(false, "Dismiss");
+		switch(state) {
+			case IDLE:
+				boolean dismiss = true;
+				for(Npc n : ctx.npcs) {
+					if(n.name().equals("Genie") ||
+							n.name().equals("Drunken dwarf")) {
+						dismiss = false;
+						break;
+					}
+				}
+				ctx.npcs.peek().interact(!dismiss,
+						(dismiss ? "Dismiss" : "Talk-to"),
+						ctx.npcs.peek().name());
+				if(!dismiss)
+					state = State.TALKING;
+				return;
+			
+			case TALKING:
+				if(!ctx.chat.chatting()) {
+					state = State.IDLE;
+					return;
+				}
+				ctx.chat.clickContinue(true);
+				return;
+		}
 	}
 }
